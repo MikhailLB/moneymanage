e8b01f7c6df5922d199b9a93ec1e980bf44a9ba5
Mikhail <lobodamisha29@gmail.com>
2024-12-13 01:58:45 +0100

    fix

--- greenmanage/budgets/forms.py
+++ greenmanage/budgets/forms.py
@@ -6,3 +6,4 @@ class AddLimitForm(forms.ModelForm):
     class Meta:
         fields = ['limit']
 
+
--- greenmanage/budgets/signals.py
+++ greenmanage/budgets/signals.py
@@ -1,12 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from accounts.models import Account
 from budgets.models import Budget
 from reminders.tasks import budget_overrun
 
 
 @receiver(post_save, sender=Budget)
 def budget_post_save(sender, instance, created, **kwargs):
+    spent = round(instance.spent / Account.objects.get(user=instance.user).currency.exchange_rate, 2)
+
     if not created:
-        budget_overrun(user=instance.user, category=instance.category, limit=instance.limit, spent=instance.spent,
+        if instance.limit < spent:
+            budget_overrun(user=instance.user, category=instance.category, limit=instance.limit, spent=spent,
                        id=instance.pk)
